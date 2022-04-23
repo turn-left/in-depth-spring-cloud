08de1b013d4e97350090956a5101d4a95c650ddf
ethen <ethenyang@126.com>
2022-04-23 22:48:28 +0800

    config client update

--- eureka-discovery-client-provider/src/main/java/com/ethen/provider/controller/ConfigController.java
+++ eureka-discovery-client-provider/src/main/java/com/ethen/provider/controller/ConfigController.java
@@ -4,6 +4,8 @@ import com.ethen.common.ResponseHelper;
 import com.ethen.common.ResponseItem;
 import com.ethen.provider.config.RedisConfigProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +23,16 @@ public class ConfigController extends EnvBasicController {
     @Autowired
     private RedisConfigProperties redisConfigProperties;
 
-    @RequestMapping("/redis")
+    @Value("${redis.cluster.instance-list}")
+    private String redisInstances;
+
+    @GetMapping("/redis")
     public ResponseItem getRedisConfig() {
         return ResponseHelper.success(redisConfigProperties, env);
+    }
+
+    @GetMapping("/redis/instances")
+    public ResponseItem getRedisInstances() {
+        return ResponseHelper.success(redisInstances);
     }
 }
