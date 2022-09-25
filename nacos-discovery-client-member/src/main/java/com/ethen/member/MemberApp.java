afc005f5ee039bc1d87ddfbd550d11e7582e036a
ethenyang <ethenyang@126.com>
2022-09-25 14:47:03 +0800

    update

---
+++ nacos-discovery-client-member/src/main/java/com/ethen/member/MemberApp.java
@@ -0,0 +1,15 @@
+package com.ethen.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ethenyang@126.com
+ * @since 2022/09/25
+ */
+@SpringBootApplication
+public class MemberApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApp.class);
+    }
+}
